f672e72c979eb29cf9241ce1cc39207ab4e26d8c
Danil Shvalov <shvalovich@gmail.com>
2021-05-06 08:58:07 +1000

    add proccess.env.DB_LINK

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,7 @@
 module.exports.secretKey = process.env.NODE_ENV !== 'production' ? 'super-secret-key' : process.env.JWT_SECRET;
 
-module.exports.dbConnectionLink = 'mongodb://localhost:27017/bitfilmsdb';
+module.exports.dbConnectionLink = process.env.NODE_ENV !== 'production'
+  ? 'mongodb://localhost:27017/bitfilmsdb'
+  : process.env.DB_LINK;
 
 module.exports.frontendLinks = /.*\/\/*.ds.movies-explorer.nomoredomains.icu/;
